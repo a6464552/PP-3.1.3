37740a721cf74a3fcebc1cfa7ed0310a581b10f9
a6464552 <129376622+a6464552@users.noreply.github.com>
2023-07-25 17:26:32 +0300

    Add files via upload

---
+++ PP-3.1.3-main/PP-3.1.3-main/PP_3_1_2_Boot_Security-master/PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -0,0 +1,11 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+
+import java.util.List;
+
+@Service
+public interface RoleDao {
+    List<Role> findAll();
+}
