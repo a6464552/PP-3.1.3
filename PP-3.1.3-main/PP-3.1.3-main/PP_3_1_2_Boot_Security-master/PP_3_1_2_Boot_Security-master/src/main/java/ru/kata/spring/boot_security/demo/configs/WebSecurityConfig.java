c13c167f5e0ee249b207a51c5942311a9570fffa
a6464552 <129376622+a6464552@users.noreply.github.com>
2023-07-26 15:03:32 +0300

    Add files via upload

---
+++ PP-3.1.3-main/PP-3.1.3-main/PP_3_1_2_Boot_Security-master/PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final SuccessUserHandler successUserHandler;
+    private final UserServiceImpl userServiceImp;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImpl userServiceImp, PasswordEncoder passwordEncoder) {
+        this.successUserHandler = successUserHandler;
+        this.userServiceImp = userServiceImp;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/logout", "/login").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().successHandler(successUserHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userServiceImp).passwordEncoder(passwordEncoder);
+    }
+
+
+
+     //аутентификация userDetailService
+//    @Bean
+//    public DaoAuthenticationProvider daoAuthenticationProvider() {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setUserDetailsService(userServiceImp);
+//        return daoAuthenticationProvider;
+//    }
+}
