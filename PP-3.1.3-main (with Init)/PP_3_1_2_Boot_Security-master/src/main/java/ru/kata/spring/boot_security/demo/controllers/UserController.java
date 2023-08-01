b62a66a379b82bc2f04a99b7bf228fc4bf47157c
a6464552 <129376622+a6464552@users.noreply.github.com>
2023-08-01 10:53:15 +0300

    Add files via upload

---
+++ PP-3.1.3-main (with Init)/PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,27 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/{id}")
+    public String showCurrentUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.showById(id));
+        return "user";
+    }
+}
